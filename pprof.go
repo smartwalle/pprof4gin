82b6a3f164dd62376a5460d0b016ece55d1c1e5d
smartwalle <smartwalle@gmail.com>
2020-11-26 11:22:58 +0800

    add prefix

--- pprof.go
+++ pprof.go
@@ -3,10 +3,22 @@ package pprof4gin
 import (
 	"github.com/gin-gonic/gin"
 	"net/http/pprof"
+	"path"
+	"strings"
 )
 
-func Run(r gin.IRoutes) {
-	r.GET("/debug/pprof/*action", func(c *gin.Context) {
+func Run(prefix string, r gin.IRoutes) {
+	prefix = strings.TrimSpace(prefix)
+	if prefix != "" {
+		prefix = CleanPath(prefix)
+	}
+
+	var p = path.Join(prefix, "/debug/pprof/*action")
+
+	r.GET(p, func(c *gin.Context) {
+		var nPath = strings.TrimPrefix(c.Request.URL.Path, prefix)
+		c.Request.URL.Path = nPath
+
 		switch c.Request.URL.Path {
 		case "/debug/pprof/":
 			pprof.Index(c.Writer, c.Request)
@@ -22,4 +34,107 @@ func Run(r gin.IRoutes) {
 			pprof.Index(c.Writer, c.Request)
 		}
 	})
+}
+
+func CleanPath(p string) string {
+	// Turn empty string into "/"
+	if p == "" {
+		return "/"
+	}
+
+	n := len(p)
+	var buf []byte
+
+	// Invariants:
+	//      reading from path; r is index of next byte to process.
+	//      writing to buf; w is index of next byte to write.
+
+	// path must start with '/'
+	r := 1
+	w := 1
+
+	if p[0] != '/' {
+		r = 0
+		buf = make([]byte, n+1)
+		buf[0] = '/'
+	}
+
+	//trailing := n > 1 && p[n-1] == '/'
+
+	// A bit more clunky without a 'lazybuf' like the path package, but the loop
+	// gets completely inlined (bufApp). So in contrast to the path package this
+	// loop has no expensive function calls (except 1x make)
+
+	for r < n {
+		switch {
+		case p[r] == '/':
+			// empty path element, trailing slash is added after the end
+			r++
+
+		case p[r] == '.' && r+1 == n:
+			//trailing = true
+			r++
+
+		case p[r] == '.' && p[r+1] == '/':
+			// . element
+			r += 2
+
+		case p[r] == '.' && p[r+1] == '.' && (r+2 == n || p[r+2] == '/'):
+			// .. element: remove to last /
+			r += 3
+
+			if w > 1 {
+				// can backtrack
+				w--
+
+				if buf == nil {
+					for w > 1 && p[w] != '/' {
+						w--
+					}
+				} else {
+					for w > 1 && buf[w] != '/' {
+						w--
+					}
+				}
+			}
+
+		default:
+			// real path element.
+			// add slash if needed
+			if w > 1 {
+				bufApp(&buf, p, w, '/')
+				w++
+			}
+
+			// copy element
+			for r < n && p[r] != '/' {
+				bufApp(&buf, p, w, p[r])
+				w++
+				r++
+			}
+		}
+	}
+
+	// re-append trailing slash
+	//if trailing && w > 1 {
+	//	bufApp(&buf, p, w, '/')
+	//	w++
+	//}
+
+	if buf == nil {
+		return p[:w]
+	}
+	return string(buf[:w])
+}
+
+func bufApp(buf *[]byte, s string, w int, c byte) {
+	if *buf == nil {
+		if s[w] == c {
+			return
+		}
+
+		*buf = make([]byte, len(s))
+		copy(*buf, s[:w])
+	}
+	(*buf)[w] = c
 }
